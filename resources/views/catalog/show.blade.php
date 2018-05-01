3ed8b327dd8bf0af3a20bdcf0b6f09783b1a7a7f
Franco <franco.mana@hotmail.com>
2018-05-01 20:12:13 -0300

    DB

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -3,22 +3,22 @@
 
     <div class="row">
         <div class="col-sm-4">
-            <img src="{{$pelicula['poster']}}" alt="">
+            <img src="{{$pelicula->poster}}" alt="">
         </div>
         <div class="col-sm-8">
-            <h3>{{$pelicula['title']}}</h3>
-            <h5>Año: {{$pelicula['year']}}</h5>
-            <h5>Director: {{$pelicula['director']}}</h5>
-            <p><strong>Resumen: </strong>{{$pelicula['synopsis']}}</p>
+            <h3>{{$pelicula->title}}</h3>
+            <h5>Año: {{$pelicula->year}}</h5>
+            <h5>Director: {{$pelicula->director}}</h5>
+            <p><strong>Resumen: </strong>{{$pelicula->synopsis}}</p>
             <spam><strong>Estado: </strong></spam>
-            @if($pelicula['rented'])
+            @if($pelicula->rented)
                 <spam>Pelicula actualmente alquilada</spam><p></p>
                 <a class="btn btn-danger">Devolver pelicula</a>
             @else
                 <spam>Pelicula disponible</spam><p></p>
                 <a class="btn btn-primary">Alquilar pelicula</a>
             @endif
-            <a class="btn btn-warning" href="/catalog/edit">Editar pelicula</a>
+            <a class="btn btn-warning" href="/catalog/edit/{{$pelicula->id}}">Editar pelicula</a>
             <a type="button" class="btn btn-dark" href="/catalog">Volver al listado</a>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,6 @@ Route::get('/login', function(){
     return view('auth.login');
 });
 
-Route::get('/', function(){
+Route::get('/logout', function(){
     return 'Logout Usuario';
 });
